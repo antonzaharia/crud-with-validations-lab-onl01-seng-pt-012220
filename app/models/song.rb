186285f4c11224f7ea6bd509b9290d601956457f
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-04 18:58:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,5 +14,6 @@ class Song < ActiveRecord::Base
   def date_not_possible
     if release_year.present? && release_year > Date.today.year
       errors.add( :release_year, "release year can't be in the future")
+    end
   end
 end
